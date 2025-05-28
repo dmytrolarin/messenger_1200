26700ed40d00a68c7827fe7c59c6bfe466dc9fb7
Dima <you@example.com>
2025-05-28 20:01:27 +0200

    show message history

--- Messenger/chat_app/views.py
+++ Messenger/chat_app/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.edit import FormView
 from django.views.generic import ListView
 from .forms import MessageForm
-from .models import ChatGroup
+from .models import ChatGroup, ChatMessage
 from django.shortcuts import redirect
 
 class ChatView(FormView):
@@ -20,6 +20,8 @@ class ChatView(FormView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         chat_group_id = self.kwargs["group_id"]
+        messages = ChatMessage.objects.filter(chat_group_id = chat_group_id)
+        data["messages"] = messages
         data["chat_group"] = ChatGroup.objects.get(pk = chat_group_id)
         return data
 
